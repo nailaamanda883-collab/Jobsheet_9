0dce504e3246e9d4b86e822bee9225ae2c756b22
nailaamanda883 <nailaamanda883@gmail.com>
2026-04-24 11:31:26 +0700

    mahasiswa19

---
+++ Mahasiswa19.java
@@ -0,0 +1,20 @@
+public class Mahasiswa19 {
+    String nim;
+    String nama;
+    String kelas;
+    int nilai;
+
+    public Mahasiswa19() {
+    }
+
+    public Mahasiswa19(String nama, String nim, String kelas) {
+        this.nama = nama;
+        this.nim = nim;
+        this.kelas = kelas;
+        this.nilai = -1;
+    }
+
+    void tugasDinilai(int nilai) {
+        this.nilai = nilai;
+    }
+}
